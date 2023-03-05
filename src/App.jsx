a647839dfb98bb5478dd60d59c986c62a8b5a25c
Tathagt <112072412+Tathagt@users.noreply.github.com>
2023-03-05 23:13:46 +0530

    Events page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React from 'react';
 import Events from './Pages/Events/Events';
 
 function App() {
-  return <div>
-    <Events/>
-  </div>;
+  return (
+    <div className='bg-hero-bg md:h-screen bg-cover bg-center font-Barlow pb-2'>
+      <Events/>
+    </div>
+  )
 }
 
 export default App;
+
--- src/Pages/Events/Events.jsx
+++ src/Pages/Events/Events.jsx
@@ -1,48 +1,41 @@
 import React from 'react';
-import logo from "../../../src/Pages/Events/fractal-1654672 1.jpg";
 
 function Events() {
     return (
-    <div class="w-full h-full" src={logo}>        
-        <h1 class='text-center font-bold text-6xl pt-3.5 tracking-wider'>EVENTS</h1>
-        <div class="flex flex-wrap justify-center gap-4 mt-20">
+    <div>        
+        <h1 class='text-center font-bold text-white text-6xl pt-3.5 tracking-wider'>EVENTS</h1>
+        <div class="flex flex-wrap justify-center gap-20 mt-20">
             <div class="max-w-sm rounded-3xl overflow-hidden shadow-lg">
-                <div class="relative">
-                    <div class="absolute top-0 left-0 right-0 bottom-0 bg-gray-400 opacity-25 blur-sm"></div>
-                </div>
-                <div class="px-6 py-4">
-                    <div class="font-bold text-4xl mb-2 text-gray-800 text-center mt-10">QuantAIfusion</div>
-                            <p class="text-gray-700 text-base text-center mt-20">
+                <div class="px-6 py-4 bg-[#ffffff14] backdrop-blur-lg">
+                    <div class="font-bold text-4xl mb-2 text-white text-center mt-20">QuantAIfusion</div>
+                            <p class="text-white text-base text-center mt-20">
                                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero doloremque ad tenetur eaque ratione nesciunt vitae ducimus aliquid, iste officiis maiores corrupti cum adipisci hic at quaerat voluptas, deleniti maxime!
                             </p>
-                            <p class="text-gray-700 text-base text-center text-3xl font-bold mt-20">
+                            <p class=" text-white text-3xl text-center font-bold mt-20">
                                 Coming Soon...
                             </p>
                 </div>
             </div>
 
             <div class="max-w-sm rounded-3xl overflow-hidden shadow-lg">
-                <div class="relative">
-                    <div class="absolute top-0 bottom-0 left-0 right-0 bg-gray-400 opacity-25 blur-sm"></div>
-                </div>
-                <div class="px-6 py-4">
-                    <div class="font-bold text-4xl mb-2 text-gray-800 text-center mt-10">QuHack</div>
-                            <p class="text-gray-700 text-base text-center mt-20">
+                <div class="px-6 py-4 bg-[#ffffff14] backdrop-blur-lg">
+                    <div class="font-bold text-4xl mb-2 text-white text-center mt-20">QuHack</div>
+                            <p class="text-white text-base text-center mt-20">
                                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero doloremque ad tenetur eaque ratione nesciunt vitae ducimus aliquid, iste officiis maiores corrupti cum adipisci hic at quaerat voluptas, deleniti maxime!
                             </p>
-                            <p class="text-gray-700 text-base text-center text-3xl font-bold mt-20">
+                            <p class="text-white  text-center text-3xl font-bold mt-20">
                                 Coming Soon...
                             </p>
                 </div>
             </div>
 
             <div class="max-w-sm rounded-3xl overflow-hidden shadow-lg">
-                <div class="px-6 py-4">
-                    <div class="font-bold text-4xl mb-2 text-gray-800 text-center mt-10">Lorem Ipsum</div>
-                        <p class="text-gray-700 text-base text-center mt-20">
+                <div class="px-6 py-4  bg-[#ffffff14] backdrop-blur-lg">
+                    <div class="font-bold text-4xl mb-2 text-white text-center mt-20">Lorem Ipsum</div>
+                        <p class="text-white text-base text-center mt-20">
                             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero doloremque ad tenetur eaque ratione nesciunt vitae ducimus aliquid, iste officiis maiores corrupti cum adipisci hic at quaerat voluptas, deleniti maxime!
                         </p>
-                        <p class="text-gray-700 text-base text-center text-3xl font-bold mt-20">
+                        <p class="text-white  text-center text-3xl font-bold mt-20">
                                 Coming Soon...
                         </p>
                 </div>
