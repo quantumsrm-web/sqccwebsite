cf7defcd63dccb42b5dd9e6e519a01a1d76ff655
Tathagt <112072412+Tathagt@users.noreply.github.com>
2023-03-21 00:25:26 +0530

    done

--- src/Pages/Events/Events.jsx
+++ src/Pages/Events/Events.jsx
@@ -5,41 +5,7 @@ function Events() {
     <div>        
         <h1 class='text-center font-bold text-white text-6xl pt-3.5 tracking-wider'>EVENTS</h1>
         <div class="flex flex-wrap justify-center gap-20 mt-20">
-            <div class="max-w-sm rounded-3xl overflow-hidden shadow-lg">
-                <div class="px-6 py-4 bg-[#ffffff14] backdrop-blur-lg">
-                    <div class="font-bold text-4xl mb-2 text-white text-center mt-20">QuantAIfusion</div>
-                            <p class="text-white text-base text-center mt-20">
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero doloremque ad tenetur eaque ratione nesciunt vitae ducimus aliquid, iste officiis maiores corrupti cum adipisci hic at quaerat voluptas, deleniti maxime!
-                            </p>
-                            <p class=" text-white text-3xl text-center font-bold mt-20">
-                                Coming Soon...
-                            </p>
-                </div>
-            </div>
-
-            <div class="max-w-sm rounded-3xl overflow-hidden shadow-lg">
-                <div class="px-6 py-4 bg-[#ffffff14] backdrop-blur-lg">
-                    <div class="font-bold text-4xl mb-2 text-white text-center mt-20">QuHack</div>
-                            <p class="text-white text-base text-center mt-20">
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero doloremque ad tenetur eaque ratione nesciunt vitae ducimus aliquid, iste officiis maiores corrupti cum adipisci hic at quaerat voluptas, deleniti maxime!
-                            </p>
-                            <p class="text-white  text-center text-3xl font-bold mt-20">
-                                Coming Soon...
-                            </p>
-                </div>
-            </div>
-
-            <div class="max-w-sm rounded-3xl overflow-hidden shadow-lg">
-                <div class="px-6 py-4  bg-[#ffffff14] backdrop-blur-lg">
-                    <div class="font-bold text-4xl mb-2 text-white text-center mt-20">Lorem Ipsum</div>
-                        <p class="text-white text-base text-center mt-20">
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero doloremque ad tenetur eaque ratione nesciunt vitae ducimus aliquid, iste officiis maiores corrupti cum adipisci hic at quaerat voluptas, deleniti maxime!
-                        </p>
-                        <p class="text-white  text-center text-3xl font-bold mt-20">
-                                Coming Soon...
-                        </p>
-                </div>
-            </div>
+            
         </div>
     </div>
     );
