170362b4b86adda984b2a7b33dec91cfc933503d
Tathagt <112072412+Tathagt@users.noreply.github.com>
2023-03-21 00:32:52 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from './Components/Footer/Footer';
 import Events from './Pages/Events/Events';
 
 function App() {
   return (
     <div className='bg-hero-bg md:h-screen bg-cover bg-center font-Barlow pb-2'>
       <Events/>
+      <Footer/>
     </div>
   )
 }
