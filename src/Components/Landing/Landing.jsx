436a2fa6e285d8c7a75da54955ed802cda49450a
AYUSH <ayushrajbls@gmail.com>
2023-04-10 10:58:32 +0530

    Added domains section

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -4,16 +4,20 @@ import circles from "./assets/circles.png";
 const Landing = () => {
   return (
     <div className="bg-[#1E1E1E] text-white">
-      <div className=" ">
-        <img src={logo} alt="main logo" className="mx-auto w-52 my-10" />
-        <p className="text-center text-5xl">
+      <div className=" h-screen">
+        <img
+          src={logo}
+          alt="main logo"
+          className="relative top-[20%] mx-auto w-52 mb-10 animate-spin-slow "
+        />
+        <p className="relative text-center text-5xl top-[25%]">
           SRM QUANTUM
           <br />
           COMPUTING CLUB
         </p>
       </div>
       <div>
-        <p className="uppercase text-center font-semibold text-4xl mt-40">
+        <p className="uppercase text-center font-semibold text-4xl mt-10">
           About us
         </p>
         <p className="mt-16 max-w-7xl mx-auto text-center">
@@ -60,6 +64,48 @@ const Landing = () => {
         </div>
       </div>
       <div></div>
+      <div className="flex flex-col mx-auto my-20 gap-10 relative">
+        {/* rectangle box card */}
+        {/* 1 */}
+        <div className="text-center mx-auto font-semibold text-5xl mb-40">DOMAINS</div>
+        <div className="max-w-sm text-center bg-[#1F2933] border-2 border-green-300 rounded-3xl px-8 py-4 drop-shadow-title relative left-[20%]">
+          <p className="text-2xl">Quantum</p>
+          <p className=" mt-8 mb-12">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+            facilis explicabo voluptatibus eum doloribus optio quas magnam natus
+            enim corporis, provident a ullam voluptas error repellat? Itaque
+            placeat dignissimos accusantium?
+          </p>
+        </div>
+        {/* 2 */}
+        <div className="max-w-sm text-center bg-[#1F2933] border-2 border-green-300 rounded-3xl px-8 py-4 drop-shadow-title  relative left-[55%]">
+          <p className="text-2xl">Technical</p>
+          <p className=" mt-8 mb-12">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+            facilis explicabo voluptatibus eum doloribus optio quas magnam natus
+            enim corporis, provident a ullam voluptas error repellat? Itaque
+            placeat dignissimos accusantium?
+          </p>
+        </div>
+        <div className="max-w-sm text-center bg-[#1F2933] border-2 border-green-300 rounded-3xl px-8 py-4 drop-shadow-title  relative left-[25%]">
+          <p className="text-2xl">Corporate</p>
+          <p className=" mt-8 mb-12">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+            facilis explicabo voluptatibus eum doloribus optio quas magnam natus
+            enim corporis, provident a ullam voluptas error repellat? Itaque
+            placeat dignissimos accusantium?
+          </p>
+        </div>
+        <div className="max-w-sm text-center bg-[#1F2933] border-2 border-green-300 rounded-3xl px-8 py-4 drop-shadow-title  relative left-[65%] mb-20">
+          <p className="text-2xl">Editorial</p>
+          <p className=" mt-8 mb-12">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+            facilis explicabo voluptatibus eum doloribus optio quas magnam natus
+            enim corporis, provident a ullam voluptas error repellat? Itaque
+            placeat dignissimos accusantium?
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,17 +2,27 @@ import React from "react";
 import navlogo from "./assets/navlogo.png";
 const Navbar = () => {
   return (
-    <div className="flex bg-[#0F0F0F] text-white p-2 justify-between ">
+    <div className="flex bg-[#0F0F0F] text-white p-2 justify-between border-b-4 border-green-300 mx-auto drop-shadow-title">
       <div>
-        <img src={navlogo} alt="" className="" />
+        <img src={navlogo} alt="" className="mx-auto" />
       </div>
       <div>
-        <ul className="flex items-center gap-20 mt-3 mr-8">
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Event</li>
-          <li>Team</li>
-          <li>Contact Us</li>
+        <ul className="flex items-center gap-20 mt-3 mr-8 text-xs md:text-base">
+          <li className="hover:underline decoration-green-300  underline-offset-4">
+            Home
+          </li>
+          <li className="hover:underline decoration-green-300 underline-offset-4">
+            About Us
+          </li>
+          <li className="hover:underline decoration-green-300 underline-offset-4">
+            Event
+          </li>
+          <li className="hover:underline decoration-green-300 underline-offset-4">
+            Team
+          </li>
+          <li className="hover:underline decoration-green-300 underline-offset-4">
+            Contact Us
+          </li>
         </ul>
       </div>
     </div>
